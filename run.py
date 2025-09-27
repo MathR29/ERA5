9819dbb57aa07e6805e944bff82017141740001d
MathR29 <mrosini29@gmail.com>
2025-09-26 20:59:49 -0500

    Minor fixes

--- run.py
+++ run.py
@@ -9,12 +9,16 @@ if __name__ == "__main__":
         end_month=12,
         start_year=2022,
         end_year=2025,
-        bbox=[90,90,90,90],
+        bbox=[40.613687151061505,
+              -95.76804054400543,
+              35.99538225441254,
+              -89.09913230512305
+              ],
         variables=["2m_temperature"],
         time_zone="utc-06:00"
     )
 
     worker = partial(main.download_worker,user_key = "9530b858-ba68-430c-8a18-3d3112b45ace")
 
-    with Pool(processes=6) as pool:
+    with Pool(processes=4) as pool:
         results = pool.map(worker,tasks)
